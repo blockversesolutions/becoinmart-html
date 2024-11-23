77a52f48d210e21524af3f5a1aca0a0d4657d787
juelthpi <juelcse2019@gmail.com>
2024-11-23 23:39:26 +0600

    design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 
 jQuery(document).ready(function ($) {
+  if ($('.data-show-table').length) {  
+    $('.data-show-table').DataTable({
+        responsive: true,
+        searching: false,
+        ordering: false,
+        info: false,
+        paging: false,
+    });
+  }
     // menu bg add
     function checkScroll() {
       if ($(window).scrollTop() > 50) {
@@ -8,7 +17,6 @@ jQuery(document).ready(function ($) {
           $(".header-area").removeClass("menu-bg");
       }
     }
- 
     $(document).on("click", function (event) {
       const $div = $("#outside-click");
       const $navbarText = $("#navbarText");
@@ -16,21 +24,31 @@ jQuery(document).ready(function ($) {
         $navbarText.removeClass("show");
       }
     });
-    $(window).on('scroll', function() {
-      if ($(window).scrollTop() > 300) {
-          $('header').addClass('menu-bg-fixed');
-      } else {
-          $('header').removeClass('menu-bg-fixed');
-      }
-    });
     // Run on page load
     $(document).ready(function () {
       checkScroll();
     });
-
     // Run on scroll
     $(window).on("scroll", function () {
       checkScroll();
     });
-
 })
+function handleResize() {
+  // Select all tables with the class "mobile-view-table"
+  const elements = document.querySelectorAll('.mobile-view-table');
+  elements.forEach((element) => {
+    // Add or remove the "nowrap" class based on the window size
+    if (window.innerWidth <= 991) {
+      element.classList.add('nowrap');
+    } else {
+      element.classList.remove('nowrap');
+    }
+  });
+}
+
+// Run handleResize on page load and attach it to the resize event
+handleResize();
+window.addEventListener('resize', handleResize);
+
+
+
